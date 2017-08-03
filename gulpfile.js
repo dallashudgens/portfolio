72b671ccf6362f1dd32ee6e00ea409ed45e26fb4
Dallas Hudgens <dallashudgens@us.ibm.com>
2017-08-02 21:32:39 -0500

    updated structure to include sass, gulp, src and public directories

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+
+const sassDir = 'client-src/scss/**/*.scss';
+const publicDir = 'public/';
+
+gulp.task('sass', function () {
+  return gulp.src(sassDir)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest(publicDir + 'css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('sass:watch', function () {
+  return gulp.watch(sassDir, ['sass']);
+});
+
+
+gulp.task('browser-sync', function () {
+
+  browserSync.init({
+    server: {
+            baseDir: publicDir
+        }
+  });
+});
+
+
+gulp.task('build', ['sass']);
+gulp.task('watch', ['sass:watch']);
+gulp.task('default', ['browser-sync', 'build', 'watch']);
